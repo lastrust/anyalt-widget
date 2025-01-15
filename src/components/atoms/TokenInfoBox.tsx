96c06327c5ec9c650f04c4d5f9bc78b5d24f54dd
Mac <kgman913@gmail.com>
2025-01-15 15:06:47 +0700

    step1 ui

---
+++ src/components/atoms/TokenInfoBox.tsx
@@ -0,0 +1,27 @@
+import { Box, BoxProps, Text } from '@chakra-ui/react';
+import { FC } from 'react';
+
+type Props = BoxProps & {
+  tokenName: string;
+  chainName: string;
+};
+
+export const TokenInfoBox: FC<Props> = ({ tokenName, chainName, ...props }) => {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="space-between"
+      alignItems="flex-start"
+      gap="7px"
+      {...props}
+    >
+      <Text color="white" fontSize="16px" fontWeight="bold">
+        {tokenName}
+      </Text>
+      <Text color="white" fontSize="12px" fontWeight="regular" opacity={0.4}>
+        On {chainName}
+      </Text>
+    </Box>
+  );
+};
