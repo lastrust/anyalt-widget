9c4612ce1aa2e58f07acf62672514dcb91d796e1
Nail` <nail.halimov.rus@gmail.com>
2025-01-30 12:33:00 +0800

    chore(widget): updated ui of modals

--- src/components/standalones/modals/SlippageModal.tsx
+++ src/components/standalones/modals/SlippageModal.tsx
@@ -47,7 +47,28 @@ export const SlippageModal: FC<Props> = ({ onClose }) => {
           right="16px"
           onClick={onClose}
         >
-          Close
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+          >
+            <path
+              d="M14.375 14.3748L5.625 5.62482"
+              stroke="#919EAB"
+              strokeWidth="1.25"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M14.375 5.62482L5.625 14.3748"
+              stroke="#919EAB"
+              strokeWidth="1.25"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
         </Box>
         <Box
           display="flex"
