8e05fc5429b4ee184769aaa8e6672a208fd7f2c5
Daniel PC <raresgeo9@gmail.com>
2025-03-26 09:29:47 +0200

    fix: rerendering issue

--- src/components/screens/widget/AnyaltWidget.tsx
+++ src/components/screens/widget/AnyaltWidget.tsx
@@ -1,6 +1,14 @@
+import { BestRouteResponse } from '@anyalt/sdk';
 import { useAtomValue } from 'jotai';
 import { useMemo } from 'react';
-import { AnyaltWidgetProps } from '../../..';
+import {
+  AnyaltWidgetProps,
+  EstimateResponse,
+  ExecuteResponse,
+  Token,
+  WalletConnector,
+  WidgetTemplateType,
+} from '../../..';
 import { showStuckTransactionDialogAtom } from '../../../store/stateStore';
 import { Footer } from '../../molecules/footer/Footer';
 import { Header } from '../../molecules/header/Header';
@@ -21,6 +29,113 @@ export {
   defaultTheme as standardTheme,
 } from '../../../theme/defaultTheme';
 export { OpenModalButton } from '../../atoms/buttons/OpenModalButton';
+
+type ContentRendererProps = {
+  showPendingOperationDialog: boolean;
+  showStuckTransactionDialog: boolean;
+  setOperationToCurrentRoute: (operation: BestRouteResponse) => void;
+  walletConnector?: WalletConnector;
+  allNecessaryWalletsConnected: boolean;
+  connectWalletsOpen: () => void;
+  resetState: () => void;
+  activeStep: number;
+  loading: boolean;
+  widgetTemplate: WidgetTemplateType;
+  isValidAmountIn: boolean;
+  onConfigClick: () => void;
+  failedToFetchRoute: boolean;
+  openSlippageModal: boolean;
+  setOpenSlippageModal: (open: boolean) => void;
+  isButtonDisabled: boolean;
+  activeRoute?: BestRouteResponse;
+  areWalletsConnected: boolean;
+  onChooseRouteButtonClick: () => void;
+  onBackClick: () => void;
+  executeCallBack: (token: Token) => Promise<ExecuteResponse>;
+  onTxComplete: () => void;
+  estimateCallback: (token: Token) => Promise<EstimateResponse>;
+  onComplete: () => void;
+};
+
+const ContentRenderer = ({
+  showPendingOperationDialog,
+  showStuckTransactionDialog,
+  setOperationToCurrentRoute,
+  walletConnector,
+  allNecessaryWalletsConnected,
+  connectWalletsOpen,
+  resetState,
+  activeStep,
+  loading,
+  widgetTemplate,
+  isValidAmountIn,
+  onConfigClick,
+  failedToFetchRoute,
+  openSlippageModal,
+  setOpenSlippageModal,
+  isButtonDisabled,
+  activeRoute,
+  areWalletsConnected,
+  onChooseRouteButtonClick,
+  onBackClick,
+  executeCallBack,
+  onTxComplete,
+  estimateCallback,
+  onComplete,
+}: ContentRendererProps) => {
+  if (showPendingOperationDialog) {
+    return (
+      <PendingOperationDialog
+        setOperationToCurrentRoute={setOperationToCurrentRoute}
+        walletConnector={walletConnector}
+        allNecessaryWalletsConnected={allNecessaryWalletsConnected}
+        connectWalletsOpen={connectWalletsOpen}
+      />
+    );
+  }
+
+  if (showStuckTransactionDialog) {
+    return <StuckTransactionDialog resetState={resetState} />;
+  }
+
+  return (
+    <Stepper activeStep={activeStep}>
+      <SelectTokenStep
+        loading={loading}
+        widgetTemplate={widgetTemplate}
+        isValidAmountIn={isValidAmountIn}
+        onConfigClick={onConfigClick}
+        failedToFetchRoute={failedToFetchRoute}
+        openSlippageModal={openSlippageModal}
+        setOpenSlippageModal={setOpenSlippageModal}
+        isButtonDisabled={isButtonDisabled}
+      />
+      <ChoosingRouteStep
+        loading={loading}
+        activeRoute={activeRoute}
+        walletConnector={walletConnector}
+        failedToFetchRoute={failedToFetchRoute}
+        areWalletsConnected={areWalletsConnected}
+        onConfigClick={onConfigClick}
+        openSlippageModal={openSlippageModal}
+        setOpenSlippageModal={setOpenSlippageModal}
+        connectWalletsOpen={connectWalletsOpen}
+        onChooseRouteButtonClick={onChooseRouteButtonClick}
+      />
+      <TransactionStep
+        onBackClick={onBackClick}
+        walletConnector={walletConnector}
+        executeCallBack={executeCallBack}
+        onTxComplete={onTxComplete}
+        estimateCallback={estimateCallback}
+      />
+      <SuccessfulDepositStep
+        onConfigClick={onConfigClick}
+        onComplete={onComplete}
+      />
+    </Stepper>
+  );
+};
 
 export const AnyaltWidgetWrapper = ({
   isOpen,
@@ -71,60 +186,8 @@ export const AnyaltWidgetWrapper = ({
   const showStuckTransactionDialog = useAtomValue(
     showStuckTransactionDialogAtom,
   );
-  const Content = () => {
-    switch (true) {
-      case showPendingOperationDialog:
-        return (
-          <PendingOperationDialog
-            setOperationToCurrentRoute={setOperationToCurrentRoute}
-            walletConnector={walletConnector}
-            allNecessaryWalletsConnected={allNecessaryWalletsConnected}
-            connectWalletsOpen={connectWalletsOpen}
-          />
-        );
-      case showStuckTransactionDialog:
-        return <StuckTransactionDialog resetState={resetState} />;
-      default:
-        return (
-          <Stepper activeStep={activeStep}>
-            <SelectTokenStep
-              loading={loading}
-              widgetTemplate={widgetTemplate}
-              isValidAmountIn={isValidAmountIn}
-              onConfigClick={onConfigClick}
-              failedToFetchRoute={failedToFetchRoute}
-              openSlippageModal={openSlippageModal}
-              setOpenSlippageModal={setOpenSlippageModal}
-              isButtonDisabled={isButtonDisabled}
-            />
-            <ChoosingRouteStep
-              loading={loading}
-              activeRoute={activeRoute}
-              walletConnector={walletConnector}
-              failedToFetchRoute={failedToFetchRoute}
-              areWalletsConnected={areWalletsConnected}
-              onConfigClick={onConfigClick}
-              openSlippageModal={openSlippageModal}
-              setOpenSlippageModal={setOpenSlippageModal}
-              connectWalletsOpen={connectWalletsOpen}
-              onChooseRouteButtonClick={onChooseRouteButtonClick}
-            />
-            <TransactionStep
-              onBackClick={onBackClick}
-              walletConnector={walletConnector}
-              executeCallBack={executeCallBack}
-              onTxComplete={onTxComplete}
-              estimateCallback={estimateCallback}
-            />
-            <SuccessfulDepositStep
-              onConfigClick={onConfigClick}
-              onComplete={onComplete}
-            />
-          </Stepper>
-        );
-    }
-  };
 
+  // Memoize values that determine rendering to prevent unnecessary re-renders
   const maxWidth = useMemo(() => {
     if (showPendingOperationDialog || showStuckTransactionDialog) {
       return '976px';
@@ -135,17 +198,73 @@ export const AnyaltWidgetWrapper = ({
     return '976px';
   }, [showPendingOperationDialog, showStuckTransactionDialog, activeStep]);
 
-  const customText = useMemo(() => {
+  const headerCustomText = useMemo(() => {
     if (showPendingOperationDialog || showStuckTransactionDialog) {
       return 'Transaction';
     }
     return undefined;
-  }, [showPendingOperationDialog, showStuckTransactionDialog, activeStep]);
+  }, [showPendingOperationDialog, showStuckTransactionDialog]);
+
+  // Memoize the props for the ContentRenderer to prevent unnecessary re-renders
+  const contentProps = useMemo(
+    () => ({
+      showPendingOperationDialog,
+      showStuckTransactionDialog,
+      setOperationToCurrentRoute,
+      walletConnector,
+      allNecessaryWalletsConnected,
+      connectWalletsOpen,
+      resetState,
+      activeStep,
+      loading,
+      widgetTemplate,
+      isValidAmountIn,
+      onConfigClick,
+      failedToFetchRoute,
+      openSlippageModal,
+      setOpenSlippageModal,
+      isButtonDisabled,
+      activeRoute,
+      areWalletsConnected,
+      onChooseRouteButtonClick,
+      onBackClick,
+      executeCallBack,
+      onTxComplete,
+      estimateCallback,
+      onComplete,
+    }),
+    [
+      showPendingOperationDialog,
+      showStuckTransactionDialog,
+      setOperationToCurrentRoute,
+      walletConnector,
+      allNecessaryWalletsConnected,
+      connectWalletsOpen,
+      resetState,
+      activeStep,
+      loading,
+      widgetTemplate,
+      isValidAmountIn,
+      onConfigClick,
+      failedToFetchRoute,
+      openSlippageModal,
+      setOpenSlippageModal,
+      isButtonDisabled,
+      activeRoute,
+      areWalletsConnected,
+      onChooseRouteButtonClick,
+      onBackClick,
+      executeCallBack,
+      onTxComplete,
+      estimateCallback,
+      onComplete,
+    ],
+  );
 
   return (
     <ModalWrapper isOpen={isOpen} onClose={onClose} maxWidthCustom={maxWidth}>
-      <Header activeStep={activeStep} customText={customText} />
-      <Content />
+      <Header activeStep={activeStep} customText={headerCustomText} />
+      <ContentRenderer {...contentProps} />
       <Footer />
 
       <ConnectWalletsModal
